0e2851966c6bb56e1ea3251e8ee421b4b3659886
xuyaozuo <ca@cas-Mac-mini.local>
2014-01-27 09:31:32 +0800

    project startup

---
+++ class_define.py
@@ -0,0 +1,21 @@
+#hypothesis
+	class InputData:
+		pass
+
+	class OutputData:
+		pass
+
+	class Hypothesis:
+		pass
+
+	class Evulation:
+		pass
+
+	class SearchHypSpace:
+		pass
+
+	class HypSpace:
+		pass
+
+	class MachineLearningTest
+		pass
